523f7e6d19eee42d9215fed31c7a259620c82397
Yotfr <yotfr73@gmail.com>
2023-02-04 03:37:57 +0400

    optimized first solution

--- src/main/kotlin/leetcodeFirst.kt
+++ src/main/kotlin/leetcodeFirst.kt
@@ -1,3 +1,5 @@
+import java.util.HashMap
+
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -5,17 +7,38 @@ You can return the answer in any order.
  */
 fun main() {
     fun twoSum(nums: IntArray, target: Int): IntArray {
+
         var result: IntArray = intArrayOf()
-        nums.forEachIndexed { index, number ->
-            var diff = target - number
-            var secondIndex = nums.indexOfFirst { it == diff }
-            if (secondIndex != -1) {
-                result = intArrayOf(index, secondIndex)
+
+        var numsMap: HashMap<Int,Int> = hashMapOf()
+
+        nums.forEachIndexed { index, num ->
+            numsMap[num] = index
+        }
+        nums.forEachIndexed { index, num ->
+            var diff = target - num
+            if (numsMap.containsKey(diff) && numsMap[diff] != index && numsMap[diff] != null) {
+                result = intArrayOf(index, numsMap[diff]!!)
                 return@forEachIndexed
-            } else {
-                twoSum(nums.drop(1).toIntArray(), target)
             }
         }
         return result
     }
+}
+
+fun firstSolution(nums: IntArray, target: Int): IntArray {
+
+    var result: IntArray = intArrayOf()
+
+    nums.forEachIndexed { index, number ->
+        var diff = target - number
+        var secondIndex = nums.indexOfFirst { it == diff }
+        if (secondIndex != -1) {
+            result = intArrayOf(index, secondIndex)
+            return@forEachIndexed
+        } else {
+            firstSolution(nums.drop(1).toIntArray(), target)
+        }
+    }
+    return result
 }
